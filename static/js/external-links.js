4de4e6132a0df302ef9a1079a1d5dd56366fa1f0
CNDEV <cndev.cn@gmail.com>
2026-03-27 16:48:04 +0800

    Updata domain

--- static/js/external-links.js
+++ static/js/external-links.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelectorAll('a[href^="http"]');
     links.forEach(function(link) {
         var href = link.getAttribute('href');
-        if (href && !href.includes('blog.phpha.com')) {
+        if (href && !href.includes('xuxuejie.cn')) {
             link.setAttribute('target', '_blank');
             link.setAttribute('rel', 'noopener noreferrer');
         }
